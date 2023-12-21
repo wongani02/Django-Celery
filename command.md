e69f90998b908f25d4b666f2496c7425639ead8e
wongani02 <81077944+wongani02@users.noreply.github.com>
2023-12-21 10:31:15 +0200

    Celery cofigurations

--- command.md
+++ command.md
@@ -1,1 +1,3 @@
-docker-compose up -d --build
+docker-compose up -d --build #Run and build new containers and images
+
+docker exec -it django /bin/sh #Open up a new terminal inside our docker containercd
--- dcelery/dcelery/settings.py
+++ dcelery/dcelery/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'newapp',
 ]
 
 MIDDLEWARE = [
@@ -121,3 +122,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CELERY_BROKER_URL = os.environ.get("CELERY_BROKER_URL", "redis://redis:6379/0")
+CELERY_RESULT_BACKEND = os.environ.get("CELERY_RESULT_BACKEND", "redis://redis:6379/0")
+
